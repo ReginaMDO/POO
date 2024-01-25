735434df14e541f06636bc681775b7f31a292f76
Regina Oliveira <84687859+ReginaMDO@users.noreply.github.com>
2024-01-24 21:34:46 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,12 +3,14 @@ namespace DesafioPOO.Models
     // Herda da classe "Smartphone"
     public class Iphone : Smartphone
     {
+    public Iphone(string numero, string modelo, string marca) : base(numero, modelo, marca)
+        {
+        }
         // Sobrescreve o método "InstalarAplicativo"
-
         public override void InstalarAplicativo(string nomeApp)
         {
             // Lógica específica para instalação de aplicativo em um iPhone
-            Console.WriteLine($"Instalando {nomeApp} no iPhone {Modelo}");
+            Console.WriteLine($"{nomeApp} está sendo instalado na loja do app store do {Modelo}!");
         }
     }
 }
